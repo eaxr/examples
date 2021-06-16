bc86131feef10dbc2b8ecc50d0b981edaa5e1ef8
eaxr <61225077+eaxr@users.noreply.github.com>
2021-06-16 14:19:05 +0300

    Update synodsmTest.py

--- synodsmTest.py
+++ synodsmTest.py
@@ -13,23 +13,29 @@ class SynoZBX():
         self.user = user
         self.pasw = pasw
 
-    def auth(self):
+    def auth(self, host, port, user, pasw):
         api = SynologyDSM(self.host, self.port, self.user, self.pasw)
         api.storage.update()
+        return api
 
-    def get(self):
-        api = SynologyDSM(self.host, self.port, self.user, self.pasw)
-        api.storage.update()
+    def get(self, discovery):
+        api = self.auth(self.host, self.port, self.user, self.pasw)
         data = []
 
-        for disk_id in api.storage.disks_ids:
-            data.append({"#DRIVEID":str(api.storage.disk_name(disk_id)),
-                         "#DRIVESTATUS":str(api.storage.disk_smart_status(disk_id))})
+        if discovery == "discovery":
+            for num, disk_id in enumerate(api.storage.disks_ids):
+                data.append({"{#DRIVEID}":str(api.storage.disk_name(disk_id)),
+                             "{#DRIVENUM}":str(num)})
+        else:
+            for disk_id in api.storage.disks_ids:
+                if str(api.storage.disk_name(disk_id)) == discovery:
+                    data.append({"{#DRIVESTATUS}":str(api.storage.disk_smart_status(disk_id))})
+
         return json.dumps(data)
 
 def main():
     js = SynoZBX(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-    print(js.get())
+    print(js.get(sys.argv[5]))
 
 if __name__ == "__main__":
     main()
